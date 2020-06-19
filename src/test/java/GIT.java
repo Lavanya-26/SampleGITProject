f33cc3d4516450797a67c41c7b7bf1bfb9965bbc
Lavanya-26 <lavanya676767.gmail.com>
2020-06-19 11:10:27 +0530

    master

--- src/test/java/GIT.java
+++
@@ -1,9 +0,0 @@
-
-public class GIT {
-
-	public static void main(String[] args) {
-		System.out.println("master change");
-
-	}
-
-}
---
+++ src/test/java/org/git/SampleGIT.java
@@ -0,0 +1,10 @@
+package org.git;
+
+public class SampleGIT {
+	
+	public static void main(String[] args) {
+		System.out.println("master");
+
+	}
+
+}
